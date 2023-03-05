139eabdca68864cc8f8ba97ad73fe69139d68ca5
Niina Javanainen <niina.javanainen@gmail.com>
2023-03-05 13:05:08 +0200

    Style tooltip icons + conditional logic

--- src/components/WorldMap.tsx
+++ src/components/WorldMap.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { ComposableMap, Geographies, Geography} from "react-simple-maps";
+
 import ServiceData from '../assets/js/service-data.json';
+
 import '../assets/css/components/world-map.scss';
 
 const geoUrl = "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json";
+
+const iconOk : string = require('../assets/icons/icon-ok.svg').default;
+const iconAlert : string = require('../assets/icons/icon-alert.svg').default;
+const iconFailure : string = require('../assets/icons/icon-failure.svg').default;
 
 function WorldMap() {
   return (
@@ -44,11 +50,19 @@ function MapMarkers() {
   return (
     <div className="service-map__markers">
       {ServiceData.services.map((data) => {
-        return (
+
+        const serviceState = data.state;
+
+        return (   
           <div key={data.name} className={'service-map__marker ' + data.aws_region + ' state--' + data.state}>
-            <div className="service-map__marker__inner">
-            <p>{data.name}</p>
-            <p>{data.state}</p>
+            <div className="service-map__tooltip">
+              <div className="service-map__tooltip__icon">
+              <TooltipIcon serviceState={serviceState} />
+              </div> 
+              <div className="service-map__tooltip__content">
+                <h3>{data.name}</h3>
+                <p>{data.state}</p>
+              </div>
             </div>
             <div className={ 'service-map__state service-map__state--' + data.state}></div>
           </div>
@@ -56,6 +70,22 @@ function MapMarkers() {
       })}
     </div>
   )
+}
+
+function TooltipIcon(props: any) {
+
+  let imgSrc;
+  
+  if(props.serviceState == "ok") {
+    imgSrc = iconOk
+  } else if (props.serviceState == "alarm") {
+    imgSrc = iconAlert
+  } else {
+    imgSrc = iconFailure
+  }
+
+  return <img src={imgSrc} className={'state--' + props.serviceState} alt="" />
+
 }
 
 export default WorldMap;
