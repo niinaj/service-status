586eaaee883208812be0b62db26f4dc184918e59
Niina Javanainen <niina.javanainen@gmail.com>
2023-03-05 10:48:44 +0200

    Code cleanup + change map size

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,6 @@ import './assets/css/components/App.scss';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-          <h1>Hi world!</h1>
-          Edit <code>src/App.tsx</code> and save to reload.
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <div>
         <h2>The map</h2>
         <WorldMap />
